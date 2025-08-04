c1972a4a26ceabe6326acf5ff4828724fd568aef
Shivam <shivamy476@gmail.com>
2025-08-04 23:39:06 +0530

    Update input and output files; enhance alternating array functions and add next permutation implementations

--- t10-arrays-med2/main.cpp
+++ t10-arrays-med2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 void printArr(vector<int> &arr)
@@ -67,7 +68,6 @@ void alternatingArray(vector<int> &arr)
     {
         arr[2 * i + 1] = negArr[i];
     }
-    printArr(arr);
 }
 void alternatingArray2(vector<int> &arr)
 {
@@ -87,38 +87,142 @@ void alternatingArray2(vector<int> &arr)
             posIdx += 2;
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = res[i];
-    }
+    arr = res;
 }
 
 //? if no.(+ve) != no.(-ve)
 void alternatingArray3(vector<int> &arr)
 {
     int n = arr.size();
-    vector<int> posArr, negArr;
+    vector<int> res(n);
+    int posIdx = 0, negIdx = 1;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > 0)
-            posArr.push_back(arr[i]);
+        if (arr[i] > 0 && posIdx < n)
+        {
+            res[posIdx] = arr[i];
+            posIdx += 2;
+        }
+        else if (arr[i] < 0 && negIdx < n)
+        {
+            res[negIdx] = arr[i];
+            negIdx += 2;
+        }
+    }
+    // Fill remaining elements if any
+    for (int i = 0; i < n; i++)
+    {
+        if (posIdx < n && arr[i] > 0)
+        {
+            res[posIdx] = arr[i];
+            posIdx++;
+        }
+        else if (negIdx < n && arr[i] < 0)
+        {
+            res[negIdx] = arr[i];
+            negIdx++;
+        }
+    }
+    arr = res;
+}
+
+void nextPermutation(vector<int> &nums)
+{
+    next_permutation(nums.begin(), nums.end());
+    cout << "Done next permutation" << endl;
+}
+void nextPermutation2(vector<int> &nums)
+{
+    int n = nums.size(), k = -1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            k = i;
+            break;
+        }
+    }
+    if (k == -1)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+    else
+    {
+        for (int j = n - 1; j > k; j--)
+            if (nums[j] > nums[k])
+            {
+                swap(nums[j], nums[k]);
+                break;
+            }
+        reverse(nums.begin() + k + 1, nums.end());
+    }
+    cout << "Done next permutation" << endl;
+}
+
+void leaderInArray(vector<int> &arr)
+{
+    int n = arr.size();
+    int maxEle = INT_MIN;
+    vector<int> res;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (maxEle < arr[i])
+        {
+            maxEle = arr[i];
+            res.push_back(arr[i]);
+        }
+    }
+    for (int i = res.size() - 1; i >= 0; i--)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
+
+//? Optimal Approach
+void longestConsSequence(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    int maxSeq = 1, seq = 1, n = nums.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] = nums[i - 1] + 1)
+            seq++;
         else
-            negArr.push_back(arr[i]);
+            seq = 1;
+        maxSeq = max(maxSeq, seq);
     }
-    int m = min(posArr.size(), negArr.size());
-    for (int i = 0; i < m; i++)
+    cout << "Longest Consecutive Sequence: " << maxSeq << endl;
+}
+
+//? Using set data structure
+void longestConsSequence2(vector<int> &nums)
+{
+    unordered_set<int> s;
+    int cnt = 0, longest = 1;
+    for (int i : nums)
+        s.insert(i);
+    for (int it : s)
     {
-        arr[2 * i] = posArr[i];
-        arr[2 * i + 1] = negArr[i];
+        if (s.find(it - 1) == s.end())
+        {
+            int x = it;
+            while (s.find(x) != s.end())
+            {
+                cnt++;
+                x++;
+            }
+            longest = max(cnt, longest);
+        }
+        else
+            cnt = 0;
     }
-    for (int i = m; i < posArr.size(); i++)
-    {
-        arr[m + i] = posArr[i];
-    }
-    for (int i = m; i < negArr.size(); i++)
-    {
-        arr[m + i] = negArr[i];
-    }
+    cout << "Longest Consecutive Sequence: " << longest << endl;
+}
+
+void setMatrixZero(vector<int> arr)
+{
 }
 
 int main()
@@ -136,29 +240,58 @@ int main()
     cout << "-----------------------------------" << endl;
     vector<int> arr(n);
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i];
-    }
     printArr(arr);
     alternatingArray(arr);
     printArr(arr);
     cout << "-----------------------------------" << endl;
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i];
-    }
     printArr(arr);
     alternatingArray2(arr);
     printArr(arr);
     cout << "-----------------------------------" << endl;
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i];
-    }
     printArr(arr);
     alternatingArray3(arr);
     printArr(arr);
     cout << "-----------------------------------" << endl;
+    int m;
+    cin >> m;
+    vector<int> nums(m);
+    for (int i = 0; i < m; i++)
+        cin >> nums[i];
+    printArr(nums);
+    nextPermutation(nums);
+    printArr(nums);
+    cout << "-----------------------------------" << endl;
+    printArr(nums);
+    nextPermutation2(nums);
+    printArr(nums);
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < m; i++)
+        cin >> nums[i];
+    printArr(nums);
+    leaderInArray(nums);
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < m; i++)
+        cin >> nums[i];
+    printArr(nums);
+    longestConsSequence(nums);
+    cout << "-----------------------------------" << endl;
+    printArr(nums);
+    longestConsSequence2(nums);
+    cout << "-----------------------------------" << endl;
+    int x, y;
+    cin >> x >> y;
+    vector<vector<int>> matrix(x, vector<int>(y));
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+            cin >> matrix[x][y];
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+            cout << matrix[x][y];
 
     return 0;
 }
