ffbca3903f42830d4ca7accf2f438f6badad8fc7
Shivam <officialxshivam45@gmail.com>
2026-06-28 20:14:39 +0530

    update folder names

--- s12-t1-greedy/main.cpp
+++ s12-t1-greedy/main.cpp
@@ -20,6 +20,87 @@ int findContentChildren(vector<int> &g, vector<int> &s)
     return res;
 }
 
+struct Item
+{
+    int val;
+    int wt;
+};
+
+double fractionalKnapsack(vector<int> &val, vector<int> &wt, int capacity)
+{
+    int n = val.size();
+    vector<Item> arr(n);
+    for (int i = 0; i < n; i++)
+        arr[i] = {val[i], wt[i]};
+    sort(arr.begin(), arr.end(), [](Item &a, Item &b)
+         { return ((double)a.val / a.wt) > ((double)b.val / b.wt); });
+    double ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i].wt <= capacity)
+        {
+            ans += arr[i].val;
+            capacity -= arr[i].wt;
+        }
+        else
+        {
+            ans += (arr[i].val / (double)(arr[i].wt)) * (double)capacity;
+            break;
+        }
+    }
+    return ans;
+}
+
+bool lemonadeChange(vector<int> &bills)
+{
+    int five = 0, ten = 0;
+    for (int bill : bills)
+    {
+        if (bill == 5)
+            five++;
+        else if (bill == 10)
+        {
+            if (--five < 0)
+                return false;
+            ten++;
+        }
+        else
+        {
+            if (ten > 0 && five > 0)
+            {
+                ten--;
+                five--;
+            }
+            else if (five >= 3)
+                five -= 3;
+            else
+                return false;
+        }
+    }
+    return true;
+}
+
+bool checkValidString(string s)
+{
+    int star = 0;
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '(')
+            st.push(c);
+        else if (c == ')')
+        {
+            if (!st.empty())
+                st.pop();
+            else if (--star < 0)
+                return false;
+        }
+        else
+            star++;
+    }
+    return true;
+}
+
 int main()
 {
     int n, m;
@@ -32,7 +113,30 @@ int main()
     for (int &a : arr2)
         cin >> a;
     cout << findContentChildren(arr, arr2) << endl;
-    cout << "--------------------" << endl;
+    cout << "--------------" << endl;
+    cin >> n;
+    arr.resize(n);
+    for (int &a : arr)
+        cin >> a;
+    cin >> m;
+    arr2.resize(m);
+    for (int &a : arr2)
+        cin >> a;
+    int c;
+    cin >> c;
+    cout << fractionalKnapsack(arr, arr2, c) << endl;
+    cout << "--------------" << endl;
+    cin >> n;
+    arr.resize(n);
+    for (int &a : arr)
+        cin >> a;
+    cout << lemonadeChange(arr) << endl;
+    cout << "--------------" << endl;
+    string s;
+    getline(cin, s);
+    cout << checkValidString(s) << endl;
+    
+    cout << "--------------" << endl;
 
     return 0;
 }
